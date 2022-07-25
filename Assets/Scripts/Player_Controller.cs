fe75991c65fe75bc6a7d56e88701b796b977b58c
OutstandingBoi <antonsmirna@gmail.com>
2022-07-25 19:09:39 +0400

    Commit test

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -25,11 +25,9 @@ public class Player_Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DOTween.Init();
+        /*DOTween.Init();*/
         _rb2D = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
-
-
     }
 
     // Update is called once per frame
