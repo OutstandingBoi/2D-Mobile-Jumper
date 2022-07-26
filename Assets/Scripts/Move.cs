62e09c8b75f6cd5d35e428e9e63e8bdf6448c627
OutstandingBoi <98549581+OutstandingBoi@users.noreply.github.com>
2022-07-26 18:08:19 +0400

    Added Jump

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -5,17 +5,27 @@ using UnityEngine.Events;
 
 public class Move : MonoBehaviour
 {
+    
     Easing Easing;
     Animator Animator;
     SpriteRenderer SpriteRenderer;
     Transform Transform;
 
+    Rigidbody2D rb;
+    BoxCollider2D bc2D;
+
     bool isFacingRight = true;
+    bool isJumping = false;
+    bool isGrounded = false;
+
+    float heightTestPlayer;
     float maxSpeed = 10f;
     float moveSpeed = 2f;
-    Rigidbody2D rb;
+    public float jumpForce = 5f;
     float moveX = 0f;
     float moveY = 0f;
+    int layerMaskGround;
+
     Vector2 vZero = Vector2.zero;
     Vector2 speed = Vector2.zero;
     Vector3 pos;
@@ -29,6 +39,9 @@ public class Move : MonoBehaviour
         Animator = GetComponent<Animator>();
         SpriteRenderer = GetComponent<SpriteRenderer>();
         Transform = GetComponent<Transform>();
+        bc2D = GetComponent<BoxCollider2D>();
+        heightTestPlayer = bc2D.bounds.extents.y + 0.05f;
+        layerMaskGround = LayerMask.GetMask("Ground");
         Animator.SetBool("isIdling", true);
     }
 
@@ -41,12 +54,19 @@ public class Move : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Input.anyKey)
+        if (Input.GetAxisRaw("Horizontal") != 0)
         {
             Walk();
-        }
-        else
+        }   
+        else if (Input.GetAxisRaw("Jump") != 0 && !isJumping && IsGrounded())
         {
+            Jump();
+        }
+        else if (IsGrounded())
+        {
+            isGrounded = true;
+            isJumping = false;
+            Animator.SetBool("isJumping", false);
             Animator.SetBool("isWalking", false);
             Animator.SetBool("isIdling", true);
         }
@@ -66,8 +86,15 @@ public class Move : MonoBehaviour
         {
             Flip();
         }
-        else { }
+    }
 
+    public void Jump ()
+    {
+        isJumping = true;
+        Animator.SetBool("isIdling", false);
+        Animator.SetTrigger("isJumping");
+        moveY = Input.GetAxisRaw("Jump");
+        rb.AddForce(new Vector2(rb.velocity.x, moveY) * jumpForce, ForceMode2D.Impulse);
     }
 
 
@@ -92,11 +119,45 @@ public class Move : MonoBehaviour
         }
     }*/
 
+    //private bool IsGrounded()
+    //{
+    //    var groundCheck = Physics2D.Raycast(transform.position, Vector2.down, 0.7f);
+    //    return groundCheck.collider != null && groundCheck.collider.CompareTag("Ground");
+    //}
+
     private bool IsGrounded()
     {
-        var groundCheck = Physics2D.Raycast(transform.position, Vector2.down, 0.7f);
-        return groundCheck.collider != null && groundCheck.collider.CompareTag("Ground");
+        RaycastHit2D hit = Physics2D.Raycast(bc2D.bounds.center, Vector2.down, heightTestPlayer, layerMaskGround);
+        bool isGrounded = hit.collider != null;
+        if (isGrounded)
+        {
+            Animator.SetBool("isGrounded", true);
+        }
+        else
+        {
+            Animator.SetBool("isGrounded", false);
+        }
+        Debug.DrawRay(bc2D.bounds.center, Vector2.down * heightTestPlayer, isGrounded ? Color.green : Color.red, 0.5f);
+        
+        return isGrounded;
     }
 
+    void OnDrawGizmosSelected()
+    {
+    #if UNITY_EDITOR
+            Gizmos.color = Color.red;
+
+            //Draw the suspension
+            Gizmos.DrawLine(
+                Vector3.zero,
+                Vector3.up  
+            );
+
+            //draw force application point
+            Gizmos.DrawWireSphere(Vector3.zero, 0.05f);
+
+            Gizmos.color = Color.white;
+    #endif
+    }
 
 }
